a9937525e4f17438af0fa14e85aefb41dfcaf1b1
DEM <daniel@daniel-Inspiron-5558>
2016-08-21 21:55:58 -0400

    0.4

--- camera.py
+++ camera.py
@@ -5,20 +5,25 @@ class VideoCamera(object):
     def __init__(self):
         self.video = cv2.VideoCapture(0)
         # self.video = cv2.VideoCapture('video.mp4')
-    
+        self.frames = 0
     def __del__(self):
         self.video.release()
     
     def get_frame(self):
+        self.frames += 1
         success, image = self.video.read()
         ret, jpeg = cv2.imencode('.jpeg', image)
         jpeg = base64.b64encode(jpeg)
         return jpeg
     def stream(self, server, channel):
         while True:
-            frame = self.get_frame()
-            url = 'http://' + server + '/upload'
-            data = dict(content=frame, channel=channel)
-            r = requests.post(url, data=data, allow_redirects=True)
+            try:
+                frame = self.get_frame()
+                url = 'http://' + server + '/upload'
+                data = dict(content=frame, channel=channel)
+                r = requests.post(url, data=data, allow_redirects=True)
+            except KeyboardInterrupt:
+                print self.frames
+                exit()
 vid = VideoCamera()
-vid.stream('0.0.0.0:9400', '1') #162.243.6.91:9400
+vid.stream('162.243.6.91:9400', '1') #162.243.6.91:9400         0.0.0.0:9400
